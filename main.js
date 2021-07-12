806e5264662fb35df59c276fc99ac766fafbc43a
aaronpeet <aaronpeet@icloud.com>
2021-07-12 16:21:38 -0600

    new files/names

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import Aardvark from "./models/Aardvark.js"
 import Bandicoot from "./models/Bandicoot.js"
 import Camel from "./models/Camel.js"
 import Donkey from "./models/Donkey.js"
+import Eagle from "./models/Eagle.js"
 
 let adam = new Aardvark('adam', 'blue', 25, false, 95, 15)
  adam.eat("corndog")
@@ -17,4 +18,8 @@ cathy.speak('bark bark')
 
 let dick = new Donkey('dick', 'black', 78, false, 200, 12)
 dick.eat('hay')
-dick.speak('nay')
+dick.speak('nay') 
+
+let earl = new Eagle('earl', 'red', 10, true, 18, 4)
+earl.eat('fish')
+earl.speak('screeeeech')
