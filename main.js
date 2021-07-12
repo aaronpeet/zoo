886a2ea893928e2843a69bfc459fe371b80504ab
aaronpeet <aaronpeet@icloud.com>
2021-07-12 14:39:56 -0600

    initial push

---
+++ main.js
@@ -0,0 +1,5 @@
+import Aardvark from "./models/Aardvark.js"
+
+let adam = new Aardvark('adam', 'blue', 25, false, 95, 15)
+adam.eat("corndog")
+adam.speak("HEEEELP")
---
+++ models/Aardvark.js
@@ -0,0 +1,25 @@
+export default class Aardvark{
+    constructor(
+        name = 'alph',
+        color = 'candy-green',
+        age = 25,
+        feathers = false,
+        weight = 95,
+        tailLengthInch = 15,
+    ) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.feathers = feathers;
+        this.weight = weight;
+        this.tailLengthInch = tailLengthInch;
+}
+
+    eat(food) {
+    console.log(`Nom nom all the ${food}s`)
+    }
+    speak(noise) {
+        alert(`the ${this.name} says ${noise}!`)
+    }
+
+}
