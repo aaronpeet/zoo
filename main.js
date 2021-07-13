9ae244ee639742865e5cf751556d0ad5f96c58da
aaronpeet <aaronpeet@icloud.com>
2021-07-13 01:07:08 -0600

    commit

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import Camel from "./models/Camel.js"
 import Donkey from "./models/Donkey.js"
 import Eagle from "./models/Eagle.js"
 import Flamingo from "./models/Flamingo.js"
+import Goat from "./models/Goat.js"
 
 let adam = new Aardvark('adam', 'blue', 25, false, 95, 15)
  adam.eat("corndog")
@@ -28,3 +29,7 @@ earl.speak('screeeeech')
 let fanny = new Flamingo('fanny', 'pink', 12, true, 15, 5)
 fanny.eat('turtles')
 fanny.speak('squak')
+
+let greg = new Goat('greg', 'grey', 13, false, 85, 7)
+greg.eat('grass')
+greg.speak('meh')
